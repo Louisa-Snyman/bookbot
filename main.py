8b421936cc7342a1248e58608a3c05b2d67c1cdf
Louisa-Snyman <235snyman@gmail.com>
2025-01-02 18:12:51 +0200

    Finished booklet report

--- main.py
+++ main.py
@@ -3,7 +3,19 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     characters = count_characters(text)
-    print(characters)
+    print (f"--- Begin report of {book_path} ---")
+    print (f"{num_words} words found in the document")
+    new_dict = sort_dict(characters)
+    for key, value in new_dict.items():
+        print (f"The '{key}' character was found {value} times")
+    print ("---End report---")
+
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
 
 def count_characters(text):
     lower_string = text.lower()
@@ -16,9 +28,14 @@ def count_characters(text):
     return letters_dict
 
 
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+def sort_dict(my_dict):
+    sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[1], reverse=True))
+    new_dict = {}
+    for key, value in sorted_dict.items():
+        if key.isalpha():
+            new_dict[key] = value
+    return new_dict
+
 
 
 def get_book_text(path):
