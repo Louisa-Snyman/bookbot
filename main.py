e79588cfd545d356a62c95e6a3ba3deec1721a0f
Louisa-Snyman <235snyman@gmail.com>
2025-01-02 11:48:27 +0200

    Added count character to main

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    characters = count_characters(text)
+    print(characters)
+
+def count_characters(text):
+    lower_string = text.lower()
+    letters_dict = {}
+    for c in lower_string:
+        if c in letters_dict:
+            letters_dict[c] += 1
+        else:
+            letters_dict[c] = 1
+    return letters_dict
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
